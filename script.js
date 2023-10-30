010f3a6fc05d116f31686eb65f3b5e43a3522106
Aaron Kagan <aaronkagan@yahoo.com>
2023-10-30 17:09:24 -0400

    marking each post as read

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const markAsReadBtn = document.querySelector('.btn-header');
 const notificationCount = document.querySelector('.notification-count');
 const unreadIcons = document.querySelectorAll('.unread-icon');
 const unreadPosts = document.querySelectorAll('.unread-post');
+
 markAsReadBtn.addEventListener('click', () => {
   notificationCount.innerText = 0;
   unreadIcons.forEach((icon) => {
@@ -10,4 +11,21 @@ markAsReadBtn.addEventListener('click', () => {
   unreadPosts.forEach((post) => {
     post.classList.remove('unread-post');
   });
+});
+
+function markRead(post) {
+  post.classList.remove('unread-post');
+  notificationCount.innerText = parseInt(notificationCount.innerText) - 1;
+  console.log(
+    post.querySelector('.unread-icon').classList.remove('unread-icon')
+  );
+  post.removeEventListener('click', markReadHandler);
+}
+
+function markReadHandler() {
+  markRead(this); // Call the markRead function with the clicked element
+}
+
+unreadPosts.forEach((post) => {
+  post.addEventListener('click', markReadHandler);
 });
